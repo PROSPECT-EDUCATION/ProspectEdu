78a22091130d1e596a3a1351123dd444e2b48527
Pratima Kumari <kumaripratima337@gmail.com>
2026-01-29 11:58:15 +0530

    ok

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ import { HelmetProvider } from "react-helmet-async";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
      <HelmetProvider>
-    <BrowserRouter basename="/ProspectEdu">
+    <BrowserRouter>
     <AuthProvider>
     <OrderProvider>
   <WishlistProvider>
